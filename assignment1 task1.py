4d7c1f99bed622944c69cd29f2e52ef3e6f5232f
dkumard1998-source <dkumard1998@gmail.com>
2025-11-21 22:19:58 +0530

    Add files via upload

---
+++ assignment1 task1.py
@@ -0,0 +1,11 @@
+#Suppose the two numbers are a,b
+a=float(input("Enter first number:"))
+b=float(input("Enter second number:"))
+#Addition
+print("Addition:",a+b)
+#Substraction
+print("Substraction:",a-b)
+#Multiplication
+print("MultiplicaTION:",a*b)
+#Division
+print("Division:",a/b)
